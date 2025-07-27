52de620ea93261d37a40a8d5c36c2bfd620a6949
DANGOKYOUDAI-Tr <tanakarei.itoshima@gmail.com>
2025-07-27 10:25:12 +0900

    feat: Add delete friend functionality to server

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const { MongoClient } = require('mongodb');
 const app = express();
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
+
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*", methods: ["GET", "POST"] } });
 const PORT = process.env.PORT || 3001;
