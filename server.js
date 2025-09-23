f8c1ed2e55984e77c1c963a253aacd1131d927e9
DANGOKYOUDAI-Tr <tanakarei.itoshima@gmail.com>
2025-09-23 13:47:24 +0900

    Fix: Handle file messages correctly on server

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.use(cors());
 app.use(express.json({limit: '50mb'}));
 
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*", methods: ["GET", "POST"] } });
+const io = new Server(server, { 
+    cors: { origin: "*", methods: ["GET", "POST"] },
+    maxHttpBufferSize: 50e6 // 最大50MBのデータまで受け取れるようにする
+});
 const PORT = process.env.PORT || 3001;
 
 const MONGO_URI = process.env.MONGO_URI;
